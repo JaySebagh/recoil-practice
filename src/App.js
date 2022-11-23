0e099892e8dddeddff908c45ab124a5f40c7be3d
Jay <jaysebagh@gmail.com>
2022-11-22 16:22:16 -0800

    used atom and useRecoilState to manage api data and rendered films info

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
 import { useEffect } from 'react';
+import { atom, useRecoilState } from 'recoil'
+
+import './App.css';
+
+const filmsState = atom({
+    key: 'films',
+    default: []
+})
 
 function App() {
+    const [films, setFilms] = useRecoilState(filmsState);
+
     useEffect(() => {
         const getRepos = async () => {
             const url = "https://ghibliapi.herokuapp.com/films";
             const res = await fetch(url);
             const body = await res.json();
-            console.log(body)
+            setFilms(body)
         };
 
         getRepos();
-    }, []);
+    }, [setFilms]);
 
     return (
         <div>
-            hi
+            {films.map((film, i) => {
+                return <div key={i}>
+                    <div className="film">
+                        <h1>{film.title}</h1>
+                        <p className="release">({film.release_date})</p>
+                        <p>{film.director}</p>
+                    </div>
+                    <p>{film.description}</p>
+                </div>
+            })}
         </div>
     );
 }
